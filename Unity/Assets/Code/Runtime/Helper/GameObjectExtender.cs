4ee40ce7f24f7deb4694f92f8ad8fddf7a28c738
LadyEbony <jgarc129@ucr.edu>
2019-05-01 14:46:28 -0700

    Added base for pedestal.

---
+++ Unity/Assets/Code/Runtime/Helper/GameObjectExtender.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class GameObjectExtender{
+
+  public static Transform FindChildWithTag(Transform parent, string tag){
+    foreach(Transform child in parent){
+      if (child.CompareTag(tag)) {
+        return child;
+      } else if (child.childCount > 0) {
+        var result = FindChildWithTag(child, tag);
+        if (result != null) return result;
+      }
+    }
+    return null;
+  }
+
+}
--- Unity/Assets/Code/Runtime/Interactive/Button.cs
+++ Unity/Assets/Code/Runtime/Interactive/Button.cs
@@ -6,10 +6,28 @@ public class Button : WallInteractive {
 
   private InteractiveEvent bevent;
 
+  public Material OrbMaterial { get; private set; }
+  public Light OrbLight { get; private set; }
+  public readonly Color SelectColor = new Color(1f, 0f, .76f);
+  public readonly Color DeselectColor = new Color(.28f, 0f, .21f);
+
   public override void Awake() {
     base.Awake();
 
     bevent = GetComponent<InteractiveEvent>();
+
+    var etransform = GameObjectExtender.FindChildWithTag(transform, "Main Material");
+    if (etransform){
+      var ecomp = etransform.GetComponent<MeshRenderer>();
+      if (ecomp){
+        OrbMaterial = ecomp.material;   
+      }
+      var lcomp = etransform.GetComponent<Light>();
+      if (lcomp){
+        OrbLight = lcomp;
+      }
+      Deselect(null);
+    }
   }
 
   public override void Interact(PlayerController pc) {
@@ -17,10 +35,12 @@ public class Button : WallInteractive {
   }
 
   public override void Select(PlayerController pc) {
-    Material.SetColor("_Color", Color.red);
+    OrbMaterial.SetColor("_Color", SelectColor);
+    OrbLight.color = SelectColor;
   }
 
   public override void Deselect(PlayerController pc) {
-    Material.SetColor("_Color", Color.white);
+    OrbMaterial.SetColor("_Color", DeselectColor);
+    OrbLight.color = DeselectColor;
   }
 }
